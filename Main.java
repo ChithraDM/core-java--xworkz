ea647583ef168a5e16e1eddef490bb9783ad30f5
ChithraDM <chithradm1008@gmail.com>
2023-07-19 22:57:45 +0530

    adding array program(task) here

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+    static int age[] = {12, 18, 20, 34, 50};
+	
+	   public static void main(String args[]){
+	     System.out.println("Using loop: ");
+		 for(int i=0; i<age.length; i++ ){
+		 System.out.println(age[i]);
+		 }
+	   }
+}
